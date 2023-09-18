a4dd89b842aee4d484dcd4b10ebd879c8e9b4ae6
Rohith <rohithremesan507@gmail.com>
2023-09-18 18:11:46 +0530

    firestore sample write

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs,setDoc ,doc} from "firebase/firestore";
 import { db } from "./firebase/config";
 
 
@@ -11,12 +11,22 @@ function App() {
      <button onClick={()=>{
 
 const querySnapshotPromise = getDocs(collection(db, "users"));
+const docRef = doc(db, "users", "udi111");
 
 querySnapshotPromise.then((querySnapshot) => {
   querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
+    console.log( doc.data());
   })
 })
+
+const data = {
+  name: "sasi"
+};
+
+setDoc(docRef, data)
+  .then(() => {
+    console.log("Document successfully written!");
+  })
 
 
      }}>click me</button>
