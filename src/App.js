7902a2b59f457c867512c28473866abd38307b83
Rohith <rohithremesan507@gmail.com>
2023-09-18 17:13:19 +0530

    init firebase,log data form firestore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase/config";
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={()=>{
+
+const querySnapshotPromise = getDocs(collection(db, "users"));
+
+querySnapshotPromise.then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+  })
+})
+
+
+     }}>click me</button>
+      
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,31 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBtSNSlZ24AAs4axRpZYeRlLWkW5RdxNAE",
+  
+    authDomain: "sample-web-51403.firebaseapp.com",
+  
+    projectId: "sample-web-51403",
+  
+    storageBucket: "sample-web-51403.appspot.com",
+  
+    messagingSenderId: "423658128279",
+  
+    appId: "1:423658128279:web:d6c2834b9d792a3b893ad2",
+  
+    measurementId: "G-4CV7WJ9JH8"
+  
+  };
+  const Firebase = initializeApp(firebaseConfig);
+  
+
+  
+ export const db = getFirestore(Firebase);
+
+
+  
